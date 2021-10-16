99bc472e18020a2b2999dfba2c5676dcfbba3a75
Vasilis Spilka <vasspilka@gmail.com>
2021-10-16 18:35:02 +0200

    Update lib/bank/core/application.ex

--- lib/bank/core/application.ex
+++ lib/bank/core/application.ex
@@ -1,5 +1,5 @@
 defmodule Bank.Core.Application do
-  use Commanded.Application, otp_app: :bank, stream_prefix: "aa1"
+  use Commanded.Application, otp_app: :bank, stream_prefix: "myapp"
 
   router(Bank.Core.Router)
 end
